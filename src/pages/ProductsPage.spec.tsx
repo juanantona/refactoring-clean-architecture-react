4fc484e67a64fc5fc4392ae3fba3e5818b7d0ed6
Juanan Antona <juan.antonadelval@gmail.com>
2024-06-27 09:40:39 +0200

    test(Product Page): Adds render product and click actions button tests

--- src/pages/ProductsPage.spec.tsx
+++ src/pages/ProductsPage.spec.tsx
@@ -1,26 +1,68 @@
 import '@testing-library/jest-dom';
 import { describe, it } from '@jest/globals';
-import { act, render, screen } from '@testing-library/react';
+import { act, render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import { AppProvider } from '../context/AppProvider';
 import { ProductsPage } from './ProductsPage';
+import { RemoteProduct } from '../api/StoreApi';
 
-const mockFetch = jest.fn();
-const mockJsonResult = jest.fn();
-global.fetch = mockFetch.mockResolvedValue({ json: mockJsonResult });
+const product: RemoteProduct = {
+  id: 1001,
+  title: 'Mens Cotton Jacket',
+  price: 55.99,
+  description:
+    'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+  category: "men's clothing",
+  image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+  rating: {
+    rate: 4.7,
+    count: 500,
+  },
+};
+
+function buildProduct(remoteProduct: RemoteProduct): Product {
+  return {
+    id: remoteProduct.id,
+    title: remoteProduct.title,
+    image: remoteProduct.image,
+    price: remoteProduct.price.toLocaleString('en-US', {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    }),
+  };
+}
 
 describe('#ProductsPage', () => {
-  it('Should render the MainAppBar', async () => {
-    mockJsonResult.mockResolvedValue([]);
+  const mockFetch = jest.fn();
+  const mockJsonResult = jest.fn();
+  global.fetch = mockFetch.mockResolvedValue({ json: mockJsonResult });
 
-    await act(async () =>
-      render(
-        <AppProvider>
-          <ProductsPage />
-        </AppProvider>
-      )
-    );
+  it('Should showcase product id, title, price, image and status regarding the price', async () => {
+    const builtProduct = buildProduct(product);
+    const promise = Promise.resolve([builtProduct]);
+    mockJsonResult.mockResolvedValue(promise);
 
-    expect(screen.getByText('Refactoring a Clean Architecture in React')).toBeInTheDocument();
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await promise);
+
+    expect(screen.getByText(/Mens Cotton Jacket/i)).toBeInTheDocument();
+    expect(screen.getByText(/1001/i)).toBeInTheDocument();
+    expect(screen.getByText(/55.99/i)).toBeInTheDocument();
+    expect(screen.getByText(/active/i)).toBeInTheDocument();
+  });
+
+  it('Should showcase the Update price modal when click in the actions button', async () => {
+    const builtProduct = buildProduct(product);
+    const promise = Promise.resolve([builtProduct]);
+    mockJsonResult.mockResolvedValue(promise);
+
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await promise);
+
+    expect(mockFetch).toHaveBeenCalledWith('https://fakestoreapi.com/products');
+
+    const actionsButton = screen.getByTestId('MoreVertIcon');
+    fireEvent.click(actionsButton);
+    await waitFor(() => screen.getByText(/Update price/i));
   });
 });
