9a6fc15f3c05727083c84f8a4888f69deb8aa15f
Juanan Antona <juan.antonadelval@gmail.com>
2024-07-01 16:06:13 +0200

    test(Product Page): Improves and reorganises tests

--- src/pages/ProductsPage.spec.tsx
+++ src/pages/ProductsPage.spec.tsx
@@ -6,56 +6,65 @@ import { AppProvider } from '../context/AppProvider';
 import { ProductsPage } from './ProductsPage';
 import { StoreApi } from '../api/StoreApi';
 
-function buildProduct(remoteProduct: RemoteProduct): Product {
-  return {
-    id: remoteProduct.id,
-    title: remoteProduct.title,
-    image: remoteProduct.image,
-    price: remoteProduct.price.toLocaleString('en-US', {
-      maximumFractionDigits: 2,
-      minimumFractionDigits: 2,
-    }),
-  };
-}
-
-const oneProduct = ({ price = 1 }) => {
-  const remoteProduct = {
-    id: 1001,
-    title: 'Mens Cotton Jacket',
-    price,
-    description:
-      'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
-    rating: {
-      rate: 4.7,
-      count: 500,
-    },
-  };
-
-  return buildProduct(remoteProduct);
-};
+const oneProduct = ({ price = 1 }) => ({
+  id: 1001,
+  title: 'Mens Cotton Jacket',
+  price,
+  description:
+    'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+  category: "men's clothing",
+  image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+  rating: {
+    rate: 4.7,
+    count: 500,
+  },
+});
 
 describe('#ProductsPage', () => {
   const getAllSpy = jest.spyOn(StoreApi.prototype, 'getAll');
   const postSpy = jest.spyOn(StoreApi.prototype, 'post');
 
-  it('Should showcase product id, title, price and image attributes', async () => {
-    const product = oneProduct({ price: 55.99 });
-    getAllSpy.mockResolvedValueOnce([product]);
+  it('Should retrieve products from API when the oponent loads', async () => {
+    getAllSpy.mockResolvedValueOnce([]);
 
     render(<ProductsPage />, { wrapper: AppProvider });
     await act(async () => await getAllSpy.mock.results[0].value);
 
-    expect(getAllSpy).toHaveBeenCalled();
+    expect(getAllSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should showcase product id, title, and image attributes', async () => {
+    const product = oneProduct({});
+    getAllSpy.mockResolvedValueOnce([product]);
+
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await getAllSpy.mock.results[0].value);
 
     expect(screen.getByText(new RegExp(`${product.id}`, 'i'))).toBeInTheDocument();
     expect(screen.getByText(new RegExp(`${product.title}`, 'i'))).toBeInTheDocument();
     const images = screen.queryAllByRole('img');
     const productImage = images.find(img => img.src === product.image);
     expect(productImage).toBeInTheDocument();
-    expect(screen.getByText(/55.99/i)).toBeInTheDocument();
-    expect(screen.getByText(/active/i)).toBeInTheDocument();
+  });
+
+  it('Should showcase price formatted with 2 fraction digits', async () => {
+    const product = oneProduct({ price: 40 });
+    getAllSpy.mockResolvedValueOnce([product]);
+
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await getAllSpy.mock.results[0].value);
+
+    expect(screen.getByText(/40.00/i)).toBeInTheDocument();
+  });
+
+  it('Should showcase status active if the price is bigger than 0', async () => {
+    const product = oneProduct({ price: 1 });
+    getAllSpy.mockResolvedValueOnce([product]);
+
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await getAllSpy.mock.results[0].value);
+
+    expect(screen.getByText('active')).toBeInTheDocument();
   });
 
   it('Should showcase status inactive if the price is 0', async () => {
@@ -66,20 +75,6 @@ describe('#ProductsPage', () => {
     await act(async () => await getAllSpy.mock.results[0].value);
 
     expect(screen.getByText(/inactive/i)).toBeInTheDocument();
-  });
-
-  it('Should showcase the Update price modal when click in the actions button', async () => {
-    const product = oneProduct({});
-    getAllSpy.mockResolvedValueOnce([product]);
-
-    render(<ProductsPage />, { wrapper: AppProvider });
-    await act(async () => await getAllSpy.mock.results[0].value);
-
-    expect(getAllSpy).toHaveBeenCalled();
-
-    const actionsButton = screen.getByTestId('MoreVertIcon');
-    fireEvent.click(actionsButton);
-    expect(screen.getByText(/Update price/i)).toBeInTheDocument();
   });
 
   it('Should showcase user modal when clicking in the User button', async () => {
@@ -96,6 +91,18 @@ describe('#ProductsPage', () => {
     if (usersButton) fireEvent.click(usersButton);
     expect(screen.queryByText('Admin user')).toBeVisible();
     expect(screen.queryByText('Non admin user')).toBeVisible();
+  });
+
+  it('Should showcase the Update price modal when click in the actions button', async () => {
+    const product = oneProduct({});
+    getAllSpy.mockResolvedValueOnce([product]);
+
+    render(<ProductsPage />, { wrapper: AppProvider });
+    await act(async () => await getAllSpy.mock.results[0].value);
+
+    const actionsButton = screen.getByTestId('MoreVertIcon');
+    fireEvent.click(actionsButton);
+    expect(screen.getByText(/Update price/i)).toBeVisible();
   });
 
   it("Shouldn't allow to update the product price if the user is Non Admin", async () => {
