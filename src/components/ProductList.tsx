bedd68d15dfd811f10be502cdad10723dd885cc6
Juanan Antona <juan.antonadelval@gmail.com>
2024-07-05 11:45:30 +0200

    refactor(Product Page): Extracts ProductList component from ProductPage

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,123 @@
+import { useMemo } from 'react';
+import { Typography } from '@mui/material';
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridValueFormatterParams,
+} from '@mui/x-data-grid';
+import styled from '@emotion/styled';
+import { type Product } from '../pages/ProductsPage';
+
+type Props = {
+  products: Product[];
+  openUpdatePriceDialogForProductId: (id: number) => vooid;
+};
+
+type ProductStatus = 'active' | 'inactive';
+
+const baseColumn: Partial<GridColDef<Product>> = {
+  disableColumnMenu: true,
+  sortable: false,
+};
+
+export const ProductList: React.FC<Props> = props => {
+  const { products, openUpdatePriceDialogForProductId } = props;
+
+  const columns: GridColDef<Product>[] = useMemo(
+    () => [
+      { ...baseColumn, field: 'id', headerName: 'ID', width: 70 },
+      { ...baseColumn, field: 'title', headerName: 'Title', width: 600 },
+      {
+        ...baseColumn,
+        field: 'image',
+        headerName: 'Image',
+        width: 300,
+        headerAlign: 'center',
+        align: 'center',
+        renderCell: params => {
+          return <ProductImage src={params.row.image} />;
+        },
+      },
+      {
+        ...baseColumn,
+        field: 'price',
+        headerName: 'Price',
+        type: 'number',
+        width: 180,
+        headerAlign: 'center',
+        align: 'center',
+        valueFormatter: (params: GridValueFormatterParams<number>) => {
+          if (params.value == null) {
+            return '';
+          }
+          return `$${params.value}`;
+        },
+      },
+      {
+        ...baseColumn,
+        field: 'status',
+        headerName: 'Status',
+        width: 120,
+        headerAlign: 'center',
+        align: 'center',
+        renderCell: params => {
+          const status = +params.row.price === 0 ? 'inactive' : 'active';
+
+          return (
+            <StatusContainer status={status}>
+              <Typography variant="body1">{status}</Typography>
+            </StatusContainer>
+          );
+        },
+      },
+      {
+        ...baseColumn,
+
+        field: 'actions',
+        type: 'actions',
+        width: 100,
+        getActions: cell => [
+          <GridActionsCellItem
+            label="Update price"
+            onClick={() => openUpdatePriceDialogForProductId(cell.row.id)}
+            showInMenu
+          />,
+        ],
+      },
+    ],
+    [openUpdatePriceDialogForProductId]
+  );
+
+  return (
+    <DataGrid<Product>
+      rowHeight={300}
+      rows={products}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: { page: 0, pageSize: 5 },
+        },
+      }}
+      pageSizeOptions={[5, 10]}
+      columnBuffer={6}
+    />
+  );
+};
+
+const StatusContainer = styled.div<{ status: ProductStatus }>`
+  background: ${props => (props.status === 'inactive' ? 'red' : 'green')};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  padding: 8px;
+  border-radius: 20px;
+  width: 100px;
+`;
+
+const ProductImage = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: contain;
+`;
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,24 +1,15 @@
 import { Container, Stack, Typography } from '@mui/material';
-import {
-  DataGrid,
-  GridActionsCellItem,
-  GridColDef,
-  GridValueFormatterParams,
-} from '@mui/x-data-grid';
+
 import { Footer } from '../components/Footer';
 import { MainAppBar } from '../components/MainAppBar';
 import styled from '@emotion/styled';
-import { useEffect, useMemo, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import { type Notification, ToastNotification } from '../components/ToastNotification';
 import { UpdatePriceDialog } from '../components/UpdatePriceDialog';
+import { ProductList } from '../components/ProductList';
 import { useReload } from '../hooks/useReload';
 import { RemoteProduct, StoreApi } from '../api/StoreApi';
-
-const baseColumn: Partial<GridColDef<Product>> = {
-  disableColumnMenu: true,
-  sortable: false,
-};
 
 const storeApi = new StoreApi();
 
@@ -45,71 +36,6 @@ export const ProductsPage: React.FC = () => {
     setEditingProductId(id);
   }, []);
 
-  const columns: GridColDef<Product>[] = useMemo(
-    () => [
-      { ...baseColumn, field: 'id', headerName: 'ID', width: 70 },
-      { ...baseColumn, field: 'title', headerName: 'Title', width: 600 },
-      {
-        ...baseColumn,
-        field: 'image',
-        headerName: 'Image',
-        width: 300,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: params => {
-          return <ProductImage src={params.row.image} />;
-        },
-      },
-      {
-        ...baseColumn,
-        field: 'price',
-        headerName: 'Price',
-        type: 'number',
-        width: 180,
-        headerAlign: 'center',
-        align: 'center',
-        valueFormatter: (params: GridValueFormatterParams<number>) => {
-          if (params.value == null) {
-            return '';
-          }
-          return `$${params.value}`;
-        },
-      },
-      {
-        ...baseColumn,
-        field: 'status',
-        headerName: 'Status',
-        width: 120,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: params => {
-          const status = +params.row.price === 0 ? 'inactive' : 'active';
-
-          return (
-            <StatusContainer status={status}>
-              <Typography variant="body1">{status}</Typography>
-            </StatusContainer>
-          );
-        },
-      },
-      {
-        ...baseColumn,
-
-        field: 'actions',
-        type: 'actions',
-        width: 100,
-        getActions: cell => [
-          <GridActionsCellItem
-            label="Update price"
-            onClick={() => openUpdatePriceDialogForProductId(cell.row.id)}
-            showInMenu
-          />,
-        ],
-      },
-    ],
-    [openUpdatePriceDialogForProductId]
-  );
-
   return (
     <Stack direction="column" sx={{ minHeight: '100vh', overflow: 'scroll' }}>
       <MainAppBar />
@@ -118,17 +44,9 @@ export const ProductsPage: React.FC = () => {
         <Typography variant="h3" component="h1" gutterBottom>
           {'Product price updater'}
         </Typography>
-        <DataGrid<Product>
-          rowHeight={300}
-          rows={products}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          columnBuffer={6}
+        <ProductList
+          products={products}
+          openUpdatePriceDialogForProductId={openUpdatePriceDialogForProductId}
         />
       </MainContainer>
       <Footer />
@@ -156,31 +74,12 @@ const MainContainer = styled(Container)`
   flex: 1;
 `;
 
-const ProductImage = styled.img`
-  width: 200px;
-  height: 200px;
-  object-fit: contain;
-`;
-
-type ProductStatus = 'active' | 'inactive';
-
 export interface Product {
   id: number;
   title: string;
   image: string;
   price: string;
 }
-
-const StatusContainer = styled.div<{ status: ProductStatus }>`
-  background: ${props => (props.status === 'inactive' ? 'red' : 'green')};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: white;
-  padding: 8px;
-  border-radius: 20px;
-  width: 100px;
-`;
 
 function buildProduct(remoteProduct: RemoteProduct): Product {
   return {
